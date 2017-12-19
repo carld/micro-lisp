5b3450ca705584b2a5124b6b5e57e32cc74b043a
Carl <105003+carld@users.noreply.github.com>
2017-12-19 21:41:36 +1300

    Tests involving Church booleans.

--- test.sh
+++ test.sh
@@ -65,6 +65,7 @@ check-prog-ok examples/apply.lisp '(fn 1 2 3 4)'
 check-prog-ok examples/apply2.lisp '(fn (1 2 3 4))'
 check-prog-ok examples/reverse.lisp '(9 8 7 6 5 4 3 2 1)'
 check-prog-ok examples/map.lisp '((1 0) (2 0) (3 0))'
+check-prog-ok examples/lambda-logic.lisp '(true false true false)'
 
 check-repl-ok 'hello' 'carl'
 check-repl-ok '(quote (write hello))' '(write hello)'
