91e2cb129e5687d1098a78d6ba202044b216b43a
Carl <105003+carld@users.noreply.github.com>
2018-05-19 15:01:32 +1200

    Y combinator reverse example.

---
+++ examples/y-comb-reverse.lisp
@@ -0,0 +1,11 @@
+(let ((Y (lambda (fn)
+            ((lambda (h) (h h))
+               (lambda (g)
+                 (fn (lambda (x y)
+                    ((g g) x y))))))))
+
+   (let ((reverse (Y (lambda(f)
+                       (lambda (a l)
+                         (cond ((null? l) a)
+                               ((quote t) (f (cons (car l) a) (cdr l)))))))))
+      (reverse null (quote (1 2 3 4 5 6 7 8 9)))))
