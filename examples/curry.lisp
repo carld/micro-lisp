291a30e553f957c493d7b25d5d2bc4b85bde5def
Carl <105003+carld@users.noreply.github.com>
2018-05-24 21:51:54 +1200

    curry example

---
+++ examples/curry.lisp
@@ -0,0 +1,17 @@
+(let ((Y (lambda (fn)
+            ((lambda (h) (h h))
+               (lambda (g)
+                 (fn (lambda (x y)
+                    ((g g) x y))))))))
+
+   (let ((append (Y (lambda (recursive-append)
+                       (lambda (l1 l2)
+                         (cond ((null? l1) l2)
+                               ((quote t) (cons (car l1) (recursive-append (cdr l1) l2)))))))))
+
+      (let ((curry (lambda (fn . args)
+                    (lambda (x)
+                      (apply fn (append args x))))))
+
+        (let ((cons-HEAD (curry cons (quote HEAD))))
+          (cons-HEAD (quote (TAIL)))))))
